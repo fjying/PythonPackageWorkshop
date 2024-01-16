d82a79d3272e1e55f7a35567636c8097411e2071
fjying <jf3375@princeton.edu>
2024-01-15 23:16:54 -0500

    Change codes comments

--- noxfile.py
+++ noxfile.py
@@ -4,5 +4,5 @@ import nox
 @nox.session
 def tests(session):
     session.install("numpy", "pytest") # Create temporary virtual environment to run test
-    session.install(".") # install package of home directory
-    session.run("pytest")
+    session.install(".") # Install package of home directory
+    session.run("pytest") # Would automatically go to tests folder to run python file for test
